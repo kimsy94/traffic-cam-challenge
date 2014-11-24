8471d0d6fac6b6ff01365c57696af10ced38feea
Sunyoon Kim <kimsunyoon@gmail.com>
2014-11-23 19:38:30 -0800

    11/23/2014

--- js/app.js
+++ js/app.js
@@ -25,4 +25,36 @@ $(document).ready(function() {
 
     var infoWindow = new google.maps.InfoWindow();
 
+    var stations;
+    var markers = [];
+
+    $.getJSON('http://data.seattle.gov/resource/65fc-btcc.json')
+        .done(function(data) {
+            stations = data;
+
+            data.forEach(function(station, itemIndex) {
+                var marker = new google.maps.Marker({
+                    position: {
+                        lat: Number(station.location.latitude),
+                        lng: Number(station.location.longitude)
+                    },
+                    map: map
+                });
+                markers.push(marker);
+
+                google.maps.event.addListener(marker, 'click', function() {
+                    var html = '<h2>' + station.station_name + '</h2>';
+                    html += '<p>' + station.street_address + '</p>';
+
+                    infoWindow.setContent(html);
+                    infoWindow.open(map, this);
+                });
+            });
+        })
+        .fail(function(error) {
+            console.log(error);
+        })
+        .always(function() {
+            $('#ajax-loader').fadeOut();
+        });
 });
